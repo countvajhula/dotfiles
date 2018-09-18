a3c366857181867364fb38ebcc6e77c6993b2622
Siddhartha <sid@countvajhula.com>
2018-09-18 16:03:01 -0700

    hydra menu to cycle through buffers alt-tab style

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -463,6 +463,33 @@
   'save-buffer)
 
 
+;;;;;;;;;;;;;;;;;
+;; HYDRA MENUS ;;
+;;;;;;;;;;;;;;;;;
+
+
+(defun return-to-original-buffer ()
+  (interactive)
+  (switch-to-buffer original-buffer))
+
+(defhydra hydra-buffers (:idle 1.0
+			 :body-pre (setq original-buffer (current-buffer)))
+  "Cycle through buffers, Alt-tab style"
+  ("b" list-buffers "show all buffers")
+  ("n" next-buffer "next buffer")
+  ("N" previous-buffer "previous buffer")
+  ("p" previous-buffer "previous buffer")
+  ("P" next-buffer "next buffer")
+  ("h" previous-buffer "previous buffer")
+  ("l" next-buffer "next buffer")
+  ("j" next-buffer "next buffer")
+  ("k" previous-buffer "previous buffer")
+  ("<escape>" return-to-original-buffer "return to original buffer" :exit t))
+
+;; access the buffer menu via a "body" keybinding
+(global-set-key (kbd "s-b") 'hydra-buffers/body)
+
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; CUSTOMIZATION VIA "UI" BELOW ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
