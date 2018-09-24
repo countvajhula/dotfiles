781d3b80c522931acc2faed15d72c3e51b76f243
Siddhartha <sid@countvajhula.com>
2018-09-24 15:07:09 -0700

    hydra for projectile

--- .emacs.d/my-init/load-packages.el
+++ .emacs.d/my-init/load-packages.el
@@ -20,9 +20,50 @@
   (setq projectile-completion-system 'ivy)
   ;; (define-key projectile-mode-map (kbd "s-p") 'projectile-command-map)
   (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
-  (define-key projectile-mode-map
+  ;; for sublime emulation:
+  ;; (define-key projectile-mode-map
+  ;;             (kbd "s-p")
+  ;;             'projectile-find-file)
+  (defhydra hydra-projectile (:color teal
+                              :hint nil)
+    "
+     PROJECTILE: %(projectile-project-root)
+
+     Find File            Search/Tags          Buffers                Cache
+------------------------------------------------------------------------------------------
+_s-f_: file            _s_: search (grep)     _i_: Ibuffer           _c_: cache clear
+ _ff_: file dwim       _g_: update gtags      _b_: switch to buffer  _x_: remove known project
+ _fd_: file curr dir   _o_: multi-occur     _s-k_: Kill all buffers  _X_: cleanup non-existing
+  _r_: recent file                                               ^^^^_z_: cache current
+  _d_: dir
+
+"
+    ("b"   projectile-switch-to-buffer)
+    ("c"   projectile-invalidate-cache)
+    ("d"   projectile-find-dir)
+    ("s-f" projectile-find-file)
+    ("ff"  projectile-find-file-dwim)
+    ("fd"  projectile-find-file-in-directory)
+    ("g"   ggtags-update-tags)
+    ("s-g" ggtags-update-tags)
+    ("i"   projectile-ibuffer)
+    ("K"   projectile-kill-buffers)
+    ("s-k" projectile-kill-buffers)
+    ("m"   projectile-multi-occur)
+    ("o"   projectile-multi-occur)
+    ("s-p" projectile-switch-project "switch project")
+    ("p"   projectile-switch-project)
+    ("s"   projectile-grep)
+    ("s-s" projectile-grep)
+    ("r"   projectile-recentf)
+    ("x"   projectile-remove-known-project)
+    ("X"   projectile-cleanup-known-projects)
+    ("z"   projectile-cache-current-file)
+    ("`"   hydra-projectile-other-window/body "other window")
+    ("q"   nil "cancel" :color blue))
+  (define-key (current-global-map)
               (kbd "s-p")
-              'projectile-find-file)
+              'hydra-projectile/body)
   (define-key (current-global-map)
               (kbd "s-F")
               'projectile-grep))
