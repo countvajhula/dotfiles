e62f2cd32a15aac2fb69e1d7d4466e605e60ee7b
Siddhartha <sid@countvajhula.com>
2018-10-04 14:00:13 -0700

    clean up init code a bit

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,10 +1,21 @@
-(setq user-home-directory (getenv "HOME"))
-(setq user-customizations-directory (concat user-emacs-directory
-                                            "my-init/"))
-(setq user-packages-directory (concat user-emacs-directory
-                                      "my-packages/"))
-(add-to-list 'load-path user-customizations-directory)
-(add-to-list 'load-path user-packages-directory)
+(defun setup-load-path ()
+  "Set up load path for the initialization process"
+  (progn (setq user-home-directory (getenv "HOME"))
+         (setq user-customizations-directory (concat user-emacs-directory
+                                                     "my-init/"))
+         (setq user-packages-directory (concat user-emacs-directory
+                                               "my-packages/"))
+         (add-to-list 'load-path user-customizations-directory)
+         (add-to-list 'load-path user-packages-directory)
+         ;; el-get -- an alternative package manager to ELPA/MELPA,
+         ;; used for packages not on ELPA/MELPA
+         (add-to-list 'load-path (concat user-emacs-directory
+                                         "el-get/el-get"))))
+
+(setup-load-path)
+;; initialize package managers and installed packages
 (load "setup")
+;; load all configured packages
 (load "load-packages")
+;; load any customizations done via EMACS UI
 (load custom-file 'noerror)
--- .emacs.d/my-init/setup.el
+++ .emacs.d/my-init/setup.el
@@ -26,8 +26,6 @@
     (package-install 'use-package))
 
 ;; set up el-get, e.g. for packages not on ELPA/MELPA
-(add-to-list 'load-path "~/.emacs.d/el-get/el-get")
-
 (unless (require 'el-get nil t)
   (url-retrieve
    "https://raw.github.com/dimitri/el-get/master/el-get-install.el"
