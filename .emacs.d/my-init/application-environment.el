104867d470daaf3dd9a6d5735a2fa16bd195a41d
Siddhartha <sid@countvajhula.com>
2018-10-04 18:19:20 -0700

    reorganizing...

--- .emacs.d/my-init/application-environment.el
+++ .emacs.d/my-init/application-environment.el
@@ -169,10 +169,79 @@
 ;; cozy time
 (use-package fireplace)
 
+
+;;;;;;;;;;;;;;;;;;;;;;
+;; CUSTOM FUNCTIONS ;;
+;;;;;;;;;;;;;;;;;;;;;;
+
+
+(defun my-count-lines-page ()
+  "Modified from emacs's built-in count-lines-page to return a list of
+   values corresponding to the position in the page."
+  (interactive)
+  (save-excursion
+    (let ((opoint (point)) beg end
+	  total before after)
+      (forward-page)
+      (beginning-of-line)
+      (or (looking-at page-delimiter)
+	  (end-of-line))
+      (setq end (point))
+      (backward-page)
+      (setq beg (point))
+      (setq total (count-lines beg end)
+	    before (count-lines beg opoint)
+	    after (count-lines opoint end))
+      (list total before after))))
+
+(defun my-buffer-info ()
+  "get info on current buffer -- similar to Vim's C-g"
+  (interactive)
+  (-let [(total before after) (my-count-lines-page)]
+    (if (= total 0)
+	(setq bufinfo (list "-- No lines in buffer --"))
+      (progn (setq percentage (floor (* (/ (float before)
+					   total)
+					100)))
+	     (setq page-position (concat
+				  "-- "
+				  (number-to-string percentage)
+				  "%"
+				  " --"))
+	     (setq total-lines (concat
+				(number-to-string total)
+				" lines"))
+	     (setq bufinfo (list total-lines page-position))))
+    (add-to-list 'bufinfo
+		 (buffer-file-name))
+    (display-message-or-buffer (string-join bufinfo " "))))
+
+(defun xah-new-empty-buffer ()
+  "Create a new empty buffer.
+   New buffer will be named “untitled” or “untitled<2>”, “untitled<3>”, etc.
+
+   It returns the buffer (for elisp programing).
+
+   URL `http://ergoemacs.org/emacs/emacs_new_empty_buffer.html'
+   Version 2017-11-01"
+  (interactive)
+  (let (($buf (generate-new-buffer "untitled")))
+    (switch-to-buffer $buf)
+    (funcall initial-major-mode)
+    (setq buffer-offer-save t)
+    $buf
+    ))
+
+(defun my-lisp-repl ()
+  (interactive)
+  (evil-window-vsplit)
+  (evil-window-right 1)
+  (ielm))
+
+
 (use-package general
   ;; general is a package that provides various
   ;; resources and utilities for defining keybindings
-  :after my-utils
   :config
   (setq general-override-states '(insert
                                   emacs
@@ -207,7 +276,131 @@
 
 (use-package my-look-and-feel)
 
-;; TODO: reorganize these below
-(use-package my-utils)
 
-(use-package my-keybindings)
+;;;;;;;;;;;;;;;;;;;;;;;;
+;; CUSTOM KEYBINDINGS ;;
+;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+;; map Mac's Command key to Emacs/Lisp's Super key
+(setq mac-command-modifier 'super)
+;; make Fn key do Hyper [coz, why not]
+(setq mac-function-modifier 'hyper)
+
+; Note: "define-key (current-global-map)" is the same as global-set-key
+
+(define-key
+  ;; info on the current buffer
+  (current-global-map)
+  (kbd "C-c b")
+  'my-buffer-info)
+(define-key
+  ;; navigation sidebar
+  (current-global-map)
+  (kbd "C-c t")
+  'sr-speedbar-toggle)
+(define-key
+  ;; open a new empty buffer
+  (current-global-map)
+  (kbd "C-c n")
+  'xah-new-empty-buffer)
+(define-key
+  ;; drop into a shell (preserves path)
+  (current-global-map)
+  (kbd "C-c s")
+  'eshell)
+(define-key
+  ;; lookup in dictionary
+  (current-global-map)
+  (kbd "C-c d")
+  'dictionary-lookup-definition)
+(define-key
+  ;; open an elisp shell
+  (current-global-map)
+  (kbd "C-c l")
+  'my-lisp-repl)
+(define-key
+  ;; emulate caps lock -- alternative to an actual CAPS LOCK key
+  (current-global-map)
+  (kbd "C-<escape>")
+  'caps-lock-mode)
+(define-key
+  ;; calculator mode
+  (current-global-map)
+  (kbd "C-+")
+  'calc)
+
+
+;;;;;;;;;;;;;;;;;
+;; HYDRA MENUS ;;
+;;;;;;;;;;;;;;;;;
+
+
+(defun return-to-original-buffer ()
+  (interactive)
+  (switch-to-buffer original-buffer))
+
+(defhydra hydra-buffers (:idle 1.0
+			             :body-pre (setq original-buffer
+                                         (current-buffer)))
+  "Cycle through buffers, Alt-tab style"
+  ("b" list-buffers "show all buffers")
+  ("n" next-buffer "next buffer")
+  ("N" previous-buffer "previous buffer")
+  ("p" previous-buffer "previous buffer")
+  ("P" next-buffer "next buffer")
+  ("h" previous-buffer "previous buffer")
+  ("l" next-buffer "next buffer")
+  ("<escape>" return-to-original-buffer "return to original buffer" :exit t))
+
+;; access the buffer menu via a "body" keybinding
+(global-set-key (kbd "s-b") 'hydra-buffers/body)
+
+(defun current-transparency ()
+  (nth 0
+       (frame-parameter (selected-frame)
+			'alpha)))
+
+;; Set transparency of emacs
+(defun transparency (value)
+ "Sets the transparency of the frame window. 0=transparent/100=opaque"
+ (interactive "nTransparency Value 0 - 100 opaque:")
+ (set-frame-parameter (selected-frame) 'alpha (cons value value)))
+
+(defun adjust-transparency (delta)
+  "Adjust the transparency of the frame window by the configured delta,
+   in the range: 0=transparent/100=opaque"
+  (interactive)
+  (transparency (+ (current-transparency)
+		   delta)))
+
+(defun increase-transparency ()
+  "Increase frame transparency."
+  (interactive)
+  (adjust-transparency -3))
+
+(defun decrease-transparency ()
+  "Decrease frame transparency."
+  (interactive)
+  (adjust-transparency 3))
+
+(defun return-to-original-transparency ()
+  "Return to original transparency prior to making changes."
+  (interactive)
+  (transparency original-transparency))
+
+(defhydra hydra-transparency (:columns 1
+                              :body-pre (setq original-transparency
+                                              (current-transparency)))
+  "Control frame transparency"
+  ("+" decrease-transparency "decrease transparency")
+  ("-" increase-transparency "increase transparency")
+  ("k" decrease-transparency "decrease transparency")
+  ("j" increase-transparency "increase transparency")
+  ("<escape>" return-to-original-transparency "return to original transparency" :exit t))
+
+;; access the transparency menu via a "body" keybinding
+(global-set-key (kbd "s-e t") 'hydra-transparency/body)
+
+;; toggle line numbers
+(global-set-key (kbd "s-e n") 'display-line-numbers-mode)
