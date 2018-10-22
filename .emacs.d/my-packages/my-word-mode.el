565168f77c2bf1cc92a71be0577c2f741f59e25a
Siddhartha <sid@countvajhula.com>
2018-10-22 12:26:10 -0700

    rough word mode

--- .emacs.d/my-packages/my-word-mode.el
+++ .emacs.d/my-packages/my-word-mode.el
@@ -1,26 +1,50 @@
-(defun my-transpose-word-backward ()
-  "Transpose word backwards"
+(defun my-move-word-backward ()
+  "Move word backwards"
   (interactive)
-  (evil-backward-word-begin nil)
+  (evil-inner-word nil nil nil nil)
+  (evil-backward-WORD-begin nil)
   (transpose-words 1)
-  (evil-backward-word-begin nil))
+  (evil-backward-WORD-begin 2))
 
-(defun my-transpose-word-forward ()
-  "Transpose word forward"
+(defun my-move-word-forward ()
+  "Move word forward"
   (interactive)
+  (evil-forward-WORD-begin nil)
   (transpose-words 1))
+
+(defun my-move-word-down ()
+  "Move word down"
+  (interactive)
+  (evil-inner-word nil nil nil nil)
+  (setq word-end-position (point))
+  (evil-backward-WORD-begin nil)
+  (evil-delete (point) word-end-position 'exclusive nil nil)
+  (evil-next-line)
+  (evil-paste-before nil nil))
+
+(defun my-move-word-up ()
+  "Move word up"
+  (interactive)
+  (evil-inner-word nil nil nil nil)
+  (setq word-end-position (point))
+  (evil-backward-WORD-begin nil)
+  (evil-delete (point) word-end-position 'exclusive nil nil)
+  (evil-previous-line)
+  (evil-paste-before nil nil))
 
 (defhydra hydra-word (:idle 1.0
                       :columns 2)
   "Word mode"
-  ("h" evil-backward-word-begin "backward")
-  ;; ("j" evil-next-line "down")
-  ;; ("k" evil-previous-line "up")
-  ("l" evil-forward-word-begin "forward")
+  ("h" evil-backward-WORD-begin "backward")
+  ("j" evil-next-line "down")
+  ("k" evil-previous-line "up")
+  ("l" evil-forward-WORD-begin "forward")
   ("H" evil-first-non-blank "beginning")
   ("L" evil-last-non-blank "end")
-  ("C-h" my-transpose-word-backward "move left")
-  ("C-l" my-transpose-word-forward "move right")
+  ("C-h" my-move-word-backward "move left")
+  ("C-l" my-move-word-forward "move right")
+  ("C-j" my-move-word-down "move down")
+  ("C-k" my-move-word-up "move up")
   ("i" my-noop "exit" :exit t)
   ("<escape>" my-noop "exit" :exit t))
 
