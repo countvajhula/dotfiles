76054f868695cca4b81b169c75f55c866a775ae0
Siddhartha <sid@countvajhula.com>
2018-10-22 17:14:48 -0700

    more operations for word mode

--- .emacs.d/my-packages/my-word-mode.el
+++ .emacs.d/my-packages/my-word-mode.el
@@ -37,6 +37,76 @@
   (interactive)
   (apply 'evil-delete (evil-inner-word)))
 
+(defun my-toggle-case ()
+  "Toggle case"
+  (interactive)
+  (save-excursion
+    (apply 'evil-invert-case (evil-inner-word))))
+
+(defun my-upper-case ()
+  "Make upper case"
+  (interactive)
+  (save-excursion
+    (apply 'evil-upcase (evil-inner-word))))
+
+(defun my-lower-case ()
+  "Make lower case"
+  (interactive)
+  (save-excursion
+    (apply 'evil-downcase (evil-inner-word))))
+
+(defun my-split-word ()
+  "Split word into characters on separate lines"
+  (interactive)
+  (my-delete-word)
+  (evil-open-below 1)
+  (evil-force-normal-state)
+  (evil-paste-after nil nil)
+  (evil-beginning-of-line)
+  (while (not (eolp))
+    (evil-forward-char)
+    (newline)
+    (evil-force-normal-state)))
+
+(defun my-delete-other-words ()
+  "Delete other words in line"
+  (interactive)
+  (my-delete-word)
+  (evil-open-below 1)
+  (evil-force-normal-state)
+  (evil-paste-after nil nil)
+  (evil-previous-line)
+  (my-delete-line))
+
+(defun my-rotate-chars-right-in-word ()
+  "Rotate characters to the right"
+  (interactive)
+  (save-excursion
+    (let* ((word-bounds (evil-inner-word))
+           (word-start (nth 0 word-bounds))
+           (word-end (nth 1 word-bounds)))
+      (evil-delete-backward-char (- word-end 1)
+                                 word-end
+                                 'exclusive
+                                 nil)
+      (goto-char word-start)
+      (evil-paste-before nil nil))))
+
+(defun my-rotate-chars-left-in-word ()
+  "Rotate characters to the left"
+  (interactive)
+  (save-excursion
+    (let* ((word-bounds (evil-inner-word))
+           (word-start (nth 0 word-bounds))
+           (word-end (nth 1 word-bounds)))
+      (evil-delete-char word-start
+                        (+ word-start 1)
+                        'exclusive
+                        nil)
+      (goto-char (- word-end 1))
+      (evil-paste-before nil nil))))
+
+
 (defhydra hydra-word (:idle 1.0
                       :columns 2)
   "Word mode"
@@ -44,16 +114,21 @@
   ("j" evil-next-line "down")
   ("k" evil-previous-line "up")
   ("l" evil-forward-WORD-begin "forward")
-  ("H" evil-first-non-blank "beginning")
-  ("L" evil-last-non-blank "end")
+  ("H" my-rotate-chars-left-in-word "rotate chars left")
+  ("L" my-rotate-chars-right-in-word "rotate chars right")
   ("C-h" my-move-word-backward "move left")
   ("C-l" my-move-word-forward "move right")
   ("C-j" my-move-word-down "move down")
   ("C-k" my-move-word-up "move up")
   ("x" my-delete-word "delete")
-  ("d" dictionary-lookup-definition "lookup in dictionary" :exit t)
+  ("~" my-toggle-case "toggle case")
+  ("U" my-upper-case "upper case")
+  ("u" my-lower-case "lower case")
+  ("s" my-split-word "split into characters")
+  ("i" dictionary-lookup-definition "lookup in dictionary" :exit t)
+  ("s-r" my-delete-word "delete" :exit t)
+  ("s-o" my-delete-other-words "delete other words" :exit t)
   ("?" dictionary-lookup-definition "lookup in dictionary" :exit t)
-  ("i" my-noop "exit" :exit t)
   ("<escape>" my-noop "exit" :exit t))
 
 (global-set-key (kbd "s-r") 'hydra-word/body)
