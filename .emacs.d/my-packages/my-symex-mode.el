94351ef1fca867d04358b08f9b175f05095abc8d
Siddhartha <sid@countvajhula.com>
2018-11-14 22:17:17 -0800

    initial basic navigations for symex mode

--- .emacs.d/my-packages/my-symex-mode.el
+++ .emacs.d/my-packages/my-symex-mode.el
@@ -1,23 +1,51 @@
+;;; TODO: enter should visit numbers and symbols too
+;;; TODO: ideally, would be good to have a simple POC of the AST
+;;; to operate on, via semantic?
+;;; TODO: move f/b/out/in, info, yank
+
 (defun my-evaluate-symex ()
   "Evaluate Symex"
   (interactive)
-  (cond ((equal major-mode 'racket-mode)
-         (my-racket-eval-symex))
-        ((member major-mode elisp-modes)
-         (my-elisp-eval-symex))
-        ((equal major-mode 'scheme-mode)
-         (my-scheme-eval-symex))
-        (t (error "Symex mode: Lisp flavor not recognized!"))))
+  (save-excursion
+    (forward-sexp)  ;; selected symexes will have the cursor on the starting paren
+    (cond ((equal major-mode 'racket-mode)
+           (my-racket-eval-symex))
+          ((member major-mode elisp-modes)
+           (my-elisp-eval-symex))
+          ((equal major-mode 'scheme-mode)
+           (my-scheme-eval-symex))
+          (t (error "Symex mode: Lisp flavor not recognized!")))))
+
+(defun my-forward-symex ()
+  "Forward symex"
+  (interactive)
+  (forward-sexp 2)
+  (backward-sexp 1))
+
+(defun my-backward-symex ()
+  "Backward symex"
+  (interactive)
+  (backward-sexp 1))
+
+(defun my-enter-symex ()
+  "Enter lower symex level"
+  (interactive)
+  (lispy-flow 1))
+
+(defun my-exit-symex ()
+  "Exit to higher symex level"
+  (interactive)
+  (lispy--out-backward 1))
 
 (defhydra hydra-symex (:idle 1.0
                        :columns 2
                        :body-pre (evil-symex-state)
                        :post (evil-normal-state))
   "Symex mode"
-  ("h" backward-sexp "left")
-  ("j" evil-window-down "down")
-  ("k" evil-window-up "up")
-  ("l" forward-sexp "right")
+  ("h" my-backward-symex "left")
+  ("j" my-exit-symex "enter")
+  ("k" my-enter-symex "exit")
+  ("l" my-forward-symex "right")
   ("e" my-evaluate-symex "evaluate")
   ("i" my-noop "exit" :exit t)
   ("<escape>" nil "exit" :exit t)
