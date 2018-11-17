92e23bf39df93b73a1a25e6779b6cae93c9eea43
Siddhartha <sid@countvajhula.com>
2018-11-17 11:13:27 -0800

    few improvements in view mode

--- .emacs.d/my-packages/my-view-mode.el
+++ .emacs.d/my-packages/my-view-mode.el
@@ -21,6 +21,16 @@
   (interactive)
   (text-scale-adjust 0))
 
+(defun my-scroll-left ()
+  "Scroll view left"
+  (interactive)
+  (scroll-right 3))
+
+(defun my-scroll-right ()
+  "Scroll view right"
+  (interactive)
+  (scroll-left 3))
+
 (defhydra hydra-view (:idle 1.0
                       :columns 6
                       :body-pre (evil-view-state)
@@ -28,19 +38,23 @@
   "View mode"
   ("j" my-scroll-down "down")
   ("k" my-scroll-up "up")
-  ("J" evil-scroll-line-down "down fine")
-  ("K" evil-scroll-line-up "up fine")
-  ("h" evil-scroll-page-up "page up")
-  ("l" evil-scroll-page-down "page down")
-  ("H" evil-goto-first-line "beginning")
-  ("L" evil-goto-line "end")
+  ("C-S-j" evil-scroll-line-down "down fine")
+  ("C-S-k" evil-scroll-line-up "up fine")
+  ("b" evil-scroll-page-up "page up")
+  ("f" evil-scroll-page-down "page down")
+  ("h" my-scroll-left "scroll left")
+  ("l" my-scroll-right "scroll right")
+  ("g" evil-goto-first-line "beginning")
+  ("0" evil-goto-first-line "beginning")
+  ("G" evil-goto-line "end")
+  ("$" evil-goto-line "end")
   ("s-v" recenter "recenter" :exit t)
   ("C-k" my-scroll-skip-up "skip up")
   ("C-j" my-scroll-skip-down "skip down")
-  ("C-S-h" my-reset-zoom "reset zoom")
-  ("C-S-l" my-reset-zoom "reset zoom")
-  ("C-S-k" text-scale-increase "zoom in")
-  ("C-S-j" text-scale-decrease "zoom out")
+  ("H" my-reset-zoom "reset zoom")
+  ("L" my-reset-zoom "reset zoom")
+  ("K" text-scale-increase "zoom in")
+  ("J" text-scale-decrease "zoom out")
   ("u" my-scroll-half-page-up "leap up")
   ("d" my-scroll-half-page-down "leap down")
   ("i" my-noop "exit" :exit t)
