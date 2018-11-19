c480057e7518935cffe0c3637fedfe33c8bb74bd
Siddhartha <sid@countvajhula.com>
2018-11-18 23:56:23 -0800

    some handy utilities

--- .emacs.d/my-init/utils.el
+++ .emacs.d/my-init/utils.el
@@ -42,3 +42,48 @@ initial screen (when centered) -- same behavior as Vim."
                   2))
         (recenter))
       res)))
+
+(defun current-line-empty-p ()
+  "From: https://emacs.stackexchange.com/questions/16792/easiest-way-to-check-if-current-line-is-empty-ignoring-whitespace "
+  (save-excursion
+    (beginning-of-line)
+    (looking-at "[[:space:]]*$")))
+
+;;; `with-undo-collapse` macro, to treat a sequence of operations
+;;; as a single entry in the undo list.
+;;; From: https://emacs.stackexchange.com/questions/7558/collapsing-undo-history/7560#7560
+(defun undo-collapse-begin (marker)
+  "Mark the beginning of a collapsible undo block.
+This must be followed with a call to undo-collapse-end with a marker
+eq to this one."
+  (push marker buffer-undo-list))
+
+(defun undo-collapse-end (marker)
+  "Collapse undo history until a matching marker."
+  (cond
+   ((eq (car buffer-undo-list) marker)
+    (setq buffer-undo-list (cdr buffer-undo-list)))
+   (t
+    (let ((l buffer-undo-list))
+      (while (not (eq (cadr l) marker))
+        (cond
+         ((null (cdr l))
+          (error "undo-collapse-end with no matching marker"))
+         ((eq (cadr l) nil)
+          (setf (cdr l) (cddr l)))
+         (t (setq l (cdr l)))))
+      ;; remove the marker
+      (setf (cdr l) (cddr l))))))
+
+(defmacro with-undo-collapse (&rest body)
+  "Execute body, then collapse any resulting undo boundaries."
+  (declare (indent 0))
+  (let ((marker (list 'apply 'identity nil)) ; build a fresh list
+        (buffer-var (make-symbol "buffer")))
+    `(let ((,buffer-var (current-buffer)))
+       (unwind-protect
+           (progn
+             (undo-collapse-begin ',marker)
+             ,@body)
+         (with-current-buffer ,buffer-var
+           (undo-collapse-end ',marker))))))
